7f06bdca41cc4314fb9ded875a83f0c5102305ab
eli <qq1358722950@gmail.com>
2022-08-04 23:59:18 +0800

    test: use Cascadia Code fonts

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -62,11 +62,12 @@
 (defun eli/set-font ()
   (progn
     ;; Setting English Font
-    (set-face-attribute 'default nil :font "Source Code Pro 13")
-    ;; chinese fonts
+    ;; (set-face-attribute 'default nil :font "Source Code Pro 13")
+    (set-face-attribute 'default nil :font "Cascadia Code 13")
+    ;; Chinese fonts
     (dolist (charset '(kana han cjk-misc bopomofo))
       (set-fontset-font (frame-parameter nil 'font) charset
-			(font-spec :family "Sarasa Mono SC Nerd" :size 20)))
+			            (font-spec :family "Sarasa Mono SC Nerd" :size 20)))
     (set-fontset-font "fontset-default" 'unicode "AR PL New Kai" nil 'prepend)
     (set-fontset-font "fontset-default" 'unicode "Noto Color Emoji" nil 'prepend)))
 
