09b86e9c46cabd16666dc549c34e67ea2dc32052
eli <qq1358722950@gmail.com>
2022-09-18 22:57:03 +0800

    chore: update custom.el

--- custom.el
+++ custom.el
@@ -3,11 +3,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(dynamic-agenda-files '("/home/eli/Dropbox/org/roam/references/NirEyal73.org") t)
+ '(org-agenda-files
+   '("/home/eli/Dropbox/org/journal.org" "/home/eli/Dropbox/org/古文.org" "/home/eli/Dropbox/org/Français.org" "/home/eli/Dropbox/org/daily.org" "/home/eli/Dropbox/org/lists.org" "/home/eli/Dropbox/org/inbox.org" "/home/eli/Dropbox/org/words.org" "/home/eli/Dropbox/org/projects.org"))
+ '(org-download-abbreviate-filename-function 'expand-file-name)
  '(package-selected-packages
-   '(easy-hugo ox-hugo powerline mu4e-maildirs-extension mu4e-alert which-key vterm vertico smartparens shackle saveplace-pdf-view rime rainbow-delimiters quickrun pretty-hydra popwin popper org-superstar org-roam-ui org-reverse-datetree org-pdftools org-mru-clock org-latex-impatient org-download org-contrib org-clock-convenience org-appear org-anki orderless multiple-cursors mpv modern-cpp-font-lock marginalia lsp-ui lsp-latex ledger-mode kind-icon hungry-delete helpful grab-x-link flycheck-posframe expand-region ess-smart-equals embark-consult elfeed-score elfeed-org corfu citar-embark ccls cape calibredb cal-china-x bibtex-completion benchmark-init auto-yasnippet auctex all-the-icons-ibuffer all-the-icons-dired ace-window)))
+   '(toc-mode lsp-ui nov electric-operator mwim htmlize hide-mode-line visual-fill-column citar-org-roam mixed-pitch org-modern link-hint ox-pandoc powerline elfeed-org elfeed-score elfeed which-key vterm vertico smartparens shackle saveplace-pdf-view rime rainbow-delimiters quickrun pretty-hydra popwin popper org-superstar org-roam-ui org-reverse-datetree org-mru-clock org-download org-contrib org-clock-convenience org-appear org-anki orderless multiple-cursors mpv modern-cpp-font-lock marginalia lsp-latex ledger-mode kind-icon hungry-delete helpful grab-x-link flycheck-posframe expand-region ess-smart-equals embark-consult corfu citar-embark ccls cape calibredb cal-china-x bibtex-completion benchmark-init auto-yasnippet auctex all-the-icons-ibuffer all-the-icons-dired ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(lsp-ui-sideline-symbol ((t (:height 0.6 :box (:line-width (1 . -1) :color "grey") :foreground "grey"))))
+ '(lsp-ui-sideline-symbol-info ((t (:slant italic :height 0.7))))
+ '(vterm-color-white ((t (:inherit mindre-faded)))))
