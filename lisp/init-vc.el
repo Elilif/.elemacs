ec10340d55aecf8b0d8ab26a03475b2f75956c17
eli <qq1358722950@gmail.com>
2022-09-19 09:46:33 +0800

    test: add modules section in magit

--- lisp/init-vc.el
+++ lisp/init-vc.el
@@ -103,6 +103,12 @@ conventions are checked."
   (add-to-list 'git-commit-finish-query-functions
                #'my-git-commit-check-style-conventions))
 
+(with-eval-after-load 'magit
+  (magit-add-section-hook 'magit-status-sections-hook
+                          'magit-insert-modules
+                          'magit-insert-stashes
+                          'append))
+
 (elemacs-load-packages-incrementally
  '(dash f s with-editor package eieio transient git-commit))
 
