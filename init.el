3ccca20c074ac533c58cde5886a85b0e77d7c94c
eli <qq1358722950@gmail.com>
2022-10-11 16:02:07 +0800

    feat: speed up emacs startup
    Loading a whole autoload file instead of calling `(borg-initialize)`.

--- init.el
+++ init.el
@@ -45,10 +45,96 @@
 
 (setq custom-file (locate-user-emacs-file "custom.el"))
 
+;; (eval-and-compile ; `borg'
+;;   (add-to-list 'load-path (expand-file-name "lib/borg" user-emacs-directory))
+;;   (require 'borg)
+;;   (borg-initialize))
 (eval-and-compile ; `borg'
   (add-to-list 'load-path (expand-file-name "lib/borg" user-emacs-directory))
-  (require 'borg)
-  (borg-initialize))
+
+  (defun lld-collect-autoloads (file)
+    "insert all enabled drone's autoloads file to a single file."
+    (make-directory (file-name-directory file) 'parents)
+
+    ;; cleanup obsolete autoloads file
+    (dolist (f (directory-files single-autoload-path t "autoload-[0-9]+-[0-9]+\\.elc?\\'"))
+      (unless (string= file f)
+        (delete-file f)))
+
+    (message "Generating single big autoload file.")
+    (condition-case-unless-debug e
+        (with-temp-file file
+          (setq-local coding-system-for-write 'utf-8)
+          (let ((standard-output (current-buffer))
+                (print-quoted t)
+                (print-level nil)
+                (print-length nil)
+                (home (expand-file-name "~"))
+                path-list
+                theme-path-list
+                drones-path
+                auto)
+            (insert ";; -*- lexical-binding: t; coding: utf-8; no-native-compile: t -*-\n"
+                    ";; This file is generated from enabled drones.\n")
+
+            ;; replace absolute path to ~
+            (dolist (p load-path)
+              ;; collect all drone's load-path
+              (when (string-prefix-p (expand-file-name user-emacs-directory) (expand-file-name p))
+                (push p drones-path))
+
+              (if (string-prefix-p home p)
+                  (push (concat "~" (string-remove-prefix home p)) path-list)
+                (push p path-list)))
+
+            (dolist (p custom-theme-load-path)
+              (if (and (stringp p)
+                       (string-prefix-p home p))
+                  (push (concat "~" (string-remove-prefix home p)) theme-path-list)
+                (push p theme-path-list)))
+
+            (prin1 `(set `load-path ',(nreverse path-list)))
+            (insert "\n")
+            (print `(set `custom-theme-load-path ',(nreverse theme-path-list)))
+            (insert "\n")
+
+            ;; insert all drone's autoloads.el to this file
+            (dolist (p drones-path)
+              (when (file-exists-p p)
+                (setq auto (car (directory-files p t ".*-autoloads.el\\'")))
+                (when (and auto
+                           (file-exists-p auto))
+                  (insert-file-contents auto))))
+            ;; remove all #$ load code
+            (goto-char (point-min))
+            (while (re-search-forward "\(add-to-list 'load-path.*#$.*\n" nil t)
+              (replace-match ""))
+
+            ;; write local variables region
+            (goto-char (point-max))
+            (insert  "\n"
+                     "\n;; Local Variables:"
+                     "\n;; version-control: never"
+                     "\n;; no-update-autoloads: t"
+                     "\n;; End:"
+                     ))
+          t)
+      (error (delete-file file)
+             (signal 'collect-autoload-error (list file e)))))
+
+  (defvar single-autoload-path "~/.emacs.d/etc/borg/autoload/" "single autoload file.")
+  (let ((file (concat single-autoload-path
+                      "autoload-"
+                      (format-time-string
+                       "%+4Y%m%d-%H%M%S"
+                       (file-attribute-modification-time
+                        (file-attributes "/home/eli/.emacs.d/.gitmodules")))
+                      ".el")))
+    (if (file-exists-p file)
+        (load file)
+      (require 'borg)
+      (borg-initialize)
+      (lld-collect-autoloads file))))
 
 (require 'init-benchmarking)
 (require 'init-incremental-loading)
