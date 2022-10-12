740fb94943899d6ce83891ae1d4e4a65ae3e4e20
eli <qq1358722950@gmail.com>
2022-10-12 15:31:01 +0800

    fix: correct typos

--- lisp/init-better-defaults.el
+++ lisp/init-better-defaults.el
@@ -85,16 +85,16 @@
 			   ))
 
 ;; improve hippie-expand
-(setq hippie-expand-try-function-list '(try-expand-debbrev
-					try-expand-debbrev-all-buffers
-					try-expand-debbrev-from-kill
-					try-complete-file-name-partially
-					try-complete-file-name
-					try-expand-all-abbrevs
-					try-expand-list
-					try-expand-line
-					try-complete-lisp-symbol-partially
-					try-complete-lisp-symbol))
+(setq hippie-expand-try-functions-list '(try-expand-dabbrev
+					                     try-expand-dabbrev-all-buffers
+					                     try-expand-dabbrev-from-kill
+					                     try-complete-file-name-partially
+					                     try-complete-file-name
+					                     try-expand-all-abbrevs
+					                     try-expand-list
+					                     try-expand-line
+					                     try-complete-lisp-symbol-partially
+					                     try-complete-lisp-symbol))
 (global-set-key (kbd "s-/") 'hippie-expand)
 
 (setq use-short-answers t)
