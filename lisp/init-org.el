a17b0a178a5d1fe3a35856c42657866f2589229d
eli <qq1358722950@gmail.com>
2022-11-25 12:28:25 +0800

    chore: update org config

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -1791,39 +1791,40 @@ Used by `org-anki-skip-function'"
 		         ("\\subsection{%s}" . "\\subsection*{%s}")
 		         ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
 		         ("\\paragraph{%s}" . "\\paragraph*{%s}")
-		         ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+		         ("\\subparagraph{%s}" . "\\subparagraph*{%s}"))))
 
-  ;;; cross-reference in org-mode
-  (defvar org-ref-label-re
-    (rx-to-string
-     '(group-n 1 (one-or-more (any word "-.:?!`'/*@+|(){}<>&_^$#%~"))))
-    "Regexp for labels.")
+;;; cross-reference in org-mode
+(defvar org-ref-label-re
+  (rx-to-string
+   '(group-n 1 (one-or-more (any word "-.:?!`'/*@+|(){}<>&_^$#%~"))))
+  "Regexp for labels.")
 
-  (defvar org-ref-ref-label-regexps
-    (list
-     (concat ":ID:\\s-+" org-ref-label-re "\\_>")
-     ;; CUSTOM_ID in a heading
-     (concat ":CUSTOM_ID:\\s-+" org-ref-label-re "\\_>")
-     ;; #+name
-     (concat "^\\s-*#\\+name:\\s-+" org-ref-label-re "\\_>")
-     ;; labels in latex
-     (concat "\\\\label{" org-ref-label-re "}")
-     ;; A target, code copied from org-target-regexp and group 1 numbered.
-     (let ((border "[^<>\n\r \t]"))
-       (format "<<\\(?1:%s\\|%s[^<>\n\r]*%s\\)>>"
-	           border border border))
-     ;; A label link
-     (concat "label:" org-ref-label-re "\\_>")
-     "\\\\lstset{.*label=\\(?1:.*?\\),.*}")
-    "List of regular expressions to labels.
+(defvar org-ref-ref-label-regexps
+  (list
+   (concat ":ID:\\s-+" org-ref-label-re "\\_>")
+   ;; CUSTOM_ID in a heading
+   (concat ":CUSTOM_ID:\\s-+" org-ref-label-re "\\_>")
+   ;; #+name
+   (concat "^\\s-*#\\+name:\\s-+" org-ref-label-re "\\_>")
+   ;; labels in latex
+   (concat "\\\\label{" org-ref-label-re "}")
+   ;; A target, code copied from org-target-regexp and group 1 numbered.
+   (let ((border "[^<>\n\r \t]"))
+     (format "<<\\(?1:%s\\|%s[^<>\n\r]*%s\\)>>"
+	         border border border))
+   ;; A label link
+   (concat "label:" org-ref-label-re "\\_>")
+   "\\\\lstset{.*label=\\(?1:.*?\\),.*}")
+  "List of regular expressions to labels.
 The label should always be in group 1.")
 
-  (defvar-local org-ref-label-cache nil
-    "Buffer-local cache variable for labels.")
-  (defvar-local org-ref-buffer-chars-modified-tick nil
-    "Buffer-local variable to hold `buffer-chars-modified-tick'.")
-  (defun org-ref-get-labels ()
-    "Return a list of referenceable labels in the document.
+(defvar-local org-ref-label-cache nil
+  "Buffer-local cache variable for labels.")
+(defvar-local org-ref-buffer-chars-modified-tick nil
+  "Buffer-local variable to hold `buffer-chars-modified-tick'.")
+
+(defun org-ref-get-labels ()
+  "Return a list of referenceable labels in the document.
 You can reference:
 A NAME keyword
 A CUSTOM_ID property on a heading
@@ -1838,63 +1839,63 @@ Returns a list of cons cells (label . context).
 
 It is important for this function to be fast, since we use it in
 font-lock."
-    (if (or
-         ;; if we have not checked we have to check
-         (null org-ref-buffer-chars-modified-tick)
-         ;; Now check if buffer has changed since last time we looked. We check
-         ;; this with the buffer-chars-modified-tick which keeps track of changes.
-         ;; If this hasn't changed, no chars have been modified.
-         (not (= (buffer-chars-modified-tick)
-	             org-ref-buffer-chars-modified-tick)))
-        ;; We need to search for all the labels either because we don't have them,
-        ;; or the buffer has changed since we looked last time.
-        (let ((case-fold-search t)
-	          (rx (string-join org-ref-ref-label-regexps "\\|"))
-	          (labels '())
-	          oe ;; org-element
-	          context
-	          data)
-	      (save-excursion
-	        (org-with-wide-buffer
-	         (goto-char (point-min))
-	         (while (re-search-forward rx nil t)
-	           (save-match-data
-	             ;; Here we try to get some relevant context for different things you
-	             ;; might reference.
-	             (setq oe (org-element-context)
-		               context (string-trim
-			                    (pcase (car oe)
-				                  ('latex-environment
-                                   (buffer-substring
-						            (org-element-property :begin oe)
-						            (org-element-property :end oe)))
-				                  ;; figure
-				                  ('paragraph (buffer-substring
-					                           (org-element-property :begin oe)
-					                           (org-element-property :end oe)))
-				                  ('table (buffer-substring
-					                       (org-element-property :begin oe)
-					                       (org-element-property :end oe)))
-				                  ;; Headings fall here.
-				                  (_ (buffer-substring (line-beginning-position)
-						                               (line-end-position)))))))
-	           (cl-pushnew (cons (match-string-no-properties 1) context)
-			               labels))))
+  (if (or
+       ;; if we have not checked we have to check
+       (null org-ref-buffer-chars-modified-tick)
+       ;; Now check if buffer has changed since last time we looked. We check
+       ;; this with the buffer-chars-modified-tick which keeps track of changes.
+       ;; If this hasn't changed, no chars have been modified.
+       (not (= (buffer-chars-modified-tick)
+	           org-ref-buffer-chars-modified-tick)))
+      ;; We need to search for all the labels either because we don't have them,
+      ;; or the buffer has changed since we looked last time.
+      (let ((case-fold-search t)
+	        (rx (string-join org-ref-ref-label-regexps "\\|"))
+	        (labels '())
+	        oe ;; org-element
+	        context
+	        data)
+	    (save-excursion
+	      (org-with-wide-buffer
+	       (goto-char (point-min))
+	       (while (re-search-forward rx nil t)
+	         (save-match-data
+	           ;; Here we try to get some relevant context for different things you
+	           ;; might reference.
+	           (setq oe (org-element-context)
+		             context (string-trim
+			                  (pcase (car oe)
+				                ('latex-environment
+                                 (buffer-substring
+						          (org-element-property :begin oe)
+						          (org-element-property :end oe)))
+				                ;; figure
+				                ('paragraph (buffer-substring
+					                         (org-element-property :begin oe)
+					                         (org-element-property :end oe)))
+				                ('table (buffer-substring
+					                     (org-element-property :begin oe)
+					                     (org-element-property :end oe)))
+				                ;; Headings fall here.
+				                (_ (buffer-substring (line-beginning-position)
+						                             (line-end-position)))))))
+	         (cl-pushnew (cons (match-string-no-properties 1) context)
+			             labels))))
 
-	      ;; reverse so they are in the order we find them.
-	      (setq
-	       org-ref-buffer-chars-modified-tick (buffer-chars-modified-tick)
-	       org-ref-label-cache (delete-dups (reverse labels))))
+	    ;; reverse so they are in the order we find them.
+	    (setq
+	     org-ref-buffer-chars-modified-tick (buffer-chars-modified-tick)
+	     org-ref-label-cache (delete-dups (reverse labels))))
 
-      ;; retrieve the cached data
-      org-ref-label-cache))
+    ;; retrieve the cached data
+    org-ref-label-cache))
 
-  (defun eli/org-ref-insert-ref-link ()
-    "Completion function for a ref link."
-    (interactive)
-    (insert (let ((label))
-              (setq label (completing-read "label: " (org-ref-get-labels)))
-              (format "[[%s]]" label)))))
+(defun eli/org-ref-insert-ref-link ()
+  "Completion function for a ref link."
+  (interactive)
+  (insert (let ((label))
+            (setq label (completing-read "label: " (org-ref-get-labels)))
+            (format "[[%s]]" label))))
 
 ;;; org exporting config
 ;; pandoc support
