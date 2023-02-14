3e9e77572a8b2557b8c894860917128c9228811e
eli <qq1358722950@gmail.com>
2023-02-14 10:08:02 +0800

    fix: make `auto-recompile-file-maybe` buffer local

--- core/core-lib.el
+++ core/core-lib.el
@@ -81,7 +81,7 @@ the _value_ of the choice, not the selected choice. "
 	(byte-compile-file buffer-file-name)))
 
 (defun add-after-save-hook ()
-  (add-hook 'after-save-hook 'auto-recompile-file-maybe))
+  (add-hook 'after-save-hook 'auto-recompile-file-maybe nil t))
 
 (provide 'core-lib)
 ;;; core-lib.el ends here.
