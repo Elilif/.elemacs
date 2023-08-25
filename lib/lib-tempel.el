42e10a5deef42ef2c212ac4a04c7febf9340ddb1
eli <qq1358722950@gmail.com>
2023-08-25 16:15:22 +0800

    chore: update tempel config

--- lib/lib-tempel.el
+++ lib/lib-tempel.el
@@ -1,4 +1,4 @@
-;; lib-tempel.el --- Initialize lib-tempel configurations.	-*- lexical-binding: t; -*-
+;; lib-tempel.el --- Initialize lib-tempel configurations.  -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2023-2023 by Eli
 
@@ -34,14 +34,14 @@
   (when (eq (car-safe elt) 'i)
     (if-let (template (alist-get (cadr elt) (tempel--templates)))
         (cons 'l template)
-	  (message "Template %s not found" (cadr elt))
+      (message "Template %s not found" (cadr elt))
       nil)))
 
 (add-to-list 'tempel-user-elements #'tempel-include)
 
 (defun eli/abbrev--case-fixed (args)
   (when (length= args 3)
-	(setq args (append args '(nil))))
+    (setq args (append args '(nil))))
   (plist-put args :case-fixed t)
   args)
 (advice-add 'define-abbrev :filter-args #'eli/abbrev--case-fixed)
@@ -49,9 +49,9 @@
 (defun eli/tempel-expand ()
   "Complete with CAPF."
   (let ((completion-at-point-functions (list #'tempel-expand))
-		completion-cycle-threshold)
-	(tempel--save)
-	(completion-at-point)))
+        completion-cycle-threshold)
+    (tempel--save)
+    (completion-at-point)))
 
 (defun tempel-setup-capf ()
   ;; Add the Tempel Capf to `completion-at-point-functions'.
@@ -62,8 +62,8 @@
   ;; `tempel-expand' *before* the main programming mode Capf, such
   ;; that it will be tried first.
   (setq-local completion-at-point-functions
-			  (cons #'eli/tempel-expand
-					completion-at-point-functions)))
+              (cons #'eli/tempel-expand
+                    completion-at-point-functions)))
 
 (defun eli/tempel--exit (templates region name status)
   "Exit function for completion for template NAME and STATUS.
@@ -72,37 +72,23 @@ REGION are the current region bounds."
   (unless (eq status 'exact)
     (when-let ((sym (intern-soft name))
                (template (alist-get sym templates))
-			   (plist template))
-	  (while (and plist (not (keywordp (car plist))))
-		(pop plist))
+               (plist template))
+      (while (and plist (not (keywordp (car plist))))
+        (pop plist))
       (when (or (eval (plist-get plist :auto) 'lexical)
-				(eq this-command 'smarter-tab-to-expand))
-		(tempel--delete-word name)
-		(when tempel-trigger-prefix
+                (eq this-command 'smarter-tab-to-expand)
+                (eq this-command 'tempel-expand))
+        (tempel--delete-word name)
+        (when tempel-trigger-prefix
           (tempel--delete-word tempel-trigger-prefix))
-		(tempel--insert template region)))))
-
-(defun eli/bounds-of-thing-at-point (thing)
-  (let* ((bounds (bounds-of-thing-at-point thing))
-		 (a (car bounds))
-		 (b (cdr bounds))
-		 a-r b-r)
-	(when bounds
-	  (save-excursion
-		(goto-char a)
-		(setq b-r (or (1- (re-search-forward " \\|$" (line-end-position) t)) 0)))
-	  (save-excursion
-		(goto-char b)
-		(setq a-r (or (1+ (re-search-backward "^\\| " (line-beginning-position) t)) 0)))
-	  (cons (min a-r a)
-			(max b-r b)))))
+        (tempel--insert template region)))))
 
 (defun eli/tempel--get-prefix-bounds ()
   (let* ((beg (save-excursion
-				(re-search-backward " "
-									(line-beginning-position) 'noerror)))
-		 (beg (if beg (1+ beg) (line-beginning-position))))
-	(cons beg (point))))
+                (re-search-backward "\\s-"
+                                    (line-beginning-position) 'noerror)))
+         (beg (if beg (1+ beg) (line-beginning-position))))
+    (cons beg (point))))
 
 (defun tempel--prefix-bounds ()
   "Return prefix bounds."
@@ -114,86 +100,69 @@ REGION are the current region bounds."
         (when (and beg (save-excursion
                          (not (re-search-backward "\\s-" beg 'noerror))))
           (cons (+ beg (length tempel-trigger-prefix)) end)))
-    ;; (eli/bounds-of-thing-at-point 'symbol)
-	(eli/tempel--get-prefix-bounds)))
+    (eli/tempel--get-prefix-bounds)))
 
 
+
+;;; SRC: https://emacs-china.org/t/topic/21079/32?u=vagrantjoker
 (defvar smarter-tab-to-expand-in-use nil)
+(defvar eli/smarter-tab-commands '(tempel-expand org-cycle tempel-next))
 
 (defun smarter-tab-to-expand ()
   "Try to `org-cycle', `tempel-expand' at current cursor position.
 If all failed, try to complete the common part with `indent-for-tab-command'."
   (interactive)
   (if (and (not smarter-tab-to-expand-in-use)
-		   (featurep 'tempel))
+           (featurep 'tempel))
       (let ((old-point (point))
-			(old-tick (buffer-chars-modified-tick))
-			(func-list
-			 (if (equal major-mode 'org-mode) '(org-cycle tempel-expand tempel-next)
+            (old-tick (buffer-chars-modified-tick))
+            (func-list
+             (if (equal major-mode 'org-mode)
+                 eli/smarter-tab-commands
                '(tempel-expand tempel-next))))
-		(catch 'func-suceed
-		  (setq smarter-tab-to-expand-in-use t)
+        (catch 'func-suceed
+          (setq smarter-tab-to-expand-in-use t)
           (dolist (func func-list)
-			(ignore-errors (call-interactively func))
-			(unless (and (eq old-point (point))
-						 (eq old-tick (buffer-chars-modified-tick)))
-			  (setq smarter-tab-to-expand-in-use nil)
+            (ignore-errors (call-interactively func))
+            (unless (and (eq old-point (point))
+                         (eq old-tick (buffer-chars-modified-tick)))
+              (setq smarter-tab-to-expand-in-use nil)
               (throw 'func-suceed t)))
-		  (setq smarter-tab-to-expand-in-use nil)
+          (setq smarter-tab-to-expand-in-use nil)
           (indent-for-tab-command)))
-	(indent-for-tab-command)))
-
-(defun eli/org-inside-LaTeX-fragment-p ()
-  (and (fboundp #'org-inside-LaTeX-fragment-p)
-	   (org-inside-LaTeX-fragment-p)))
+    (indent-for-tab-command)))
 
 (defun eli/tempel-get-tex-object ()
   "Get tex object before point."
   (save-excursion
-	(pcase (progn
-			 (skip-chars-backward " \t" (line-beginning-position))
-			 (char-before))
-	  ((or ?\) ?\])
-	   (backward-sexp)
-	   (point))
-	  (?\}
-	   (cl-loop do (backward-sexp)
-				while (= (char-before) ?\}))
-	   (or (save-excursion
-			 (re-search-backward "\\\\" (line-beginning-position) 'noerror))
-		   (point)))
-	  ((pred (lambda (char)
-			   (not (memq char '(?\C-j)))))
-	   (backward-word)
-	   (point)))))
+    (pcase (progn
+             (skip-chars-backward " \t" (line-beginning-position))
+             (char-before))
+      ((or ?\) ?\])
+       (backward-sexp)
+       (point))
+      (?\}
+       (cl-loop do (backward-sexp)
+                while (= (char-before) ?\}))
+       (or (save-excursion
+             (re-search-backward "\\\\" (line-beginning-position) 'noerror))
+           (point)))
+      ((pred (lambda (char)
+               (not (memq char '(?\C-j)))))
+       (backward-word)
+       (point)))))
 
 (defun eli/tempel-tex-smart-kill ()
   (when-let* ((beg (eli/tempel-get-tex-object))
-			  (end (point)))
-	(kill-region beg end)))
+              (end (point)))
+    (kill-region beg end)))
 
 (defun eli/tempel-tex-smart-paste ()
   "Paste text killed by `eli/latex-smart-kill'."
   (let ((temp (string-clean-whitespace (current-kill 0))))
-	(if (string-match "^(\\(.*\\))$" temp)
-		(match-string 1 temp)
-	  temp)))
-
-(defun eli/tempel-quick-array ()
-  (let* ((bound (save-excursion
-				  (forward-line -1)
-				  (line-beginning-position)))
-		 (count (save-excursion
-				  (re-search-backward "\\\\begin{array}{\\(.*?\\)}"
-									  bound
-									  'noerror)
-				  (count-matches "[lcrp]" (match-beginning 1) (match-end 1)))))
-	
-	(if (> count 0)
-		(org-matable-create
-		 (number-to-string count)
-		 t)
-	  (org-matable-create))))
+    (if (string-match "^(\\(.*\\))$" temp)
+        (match-string 1 temp)
+      temp)))
 
 ;; C/C++ mode
 ;; (defun eli/c-fun-has-namespace-p (namespace)
@@ -240,6 +209,28 @@ If all failed, try to complete the common part with `indent-for-tab-command'."
              (list (push name template))))
       (delete-region beg end)
       (insert (symbol-name name)))))
+
+(defun eli/tempel-temp-save ()
+  "Save the selected temporary template."
+  (interactive)
+  (let* ((templates (alist-get major-mode eli/tempel-temp-templates))
+         (name (intern (completing-read "Select: " templates)))
+         (template (prin1-to-string (assoc name templates)))
+         (major (symbol-name major-mode)))
+    (with-current-buffer (find-file-noselect tempel-path)
+      (goto-char (point-min))
+      (if (search-forward major (point-max) t)
+          (progn
+            (re-search-forward "^(")
+            (backward-char)
+            (insert template)
+            (insert "\n"))
+        (search-forward ";; Local Variables:" (point-max) t)
+        (goto-char (match-beginning 0))
+        (insert major)
+        (insert "\n\n")
+        (insert template)
+        (insert "\n\n")))))
 
 (defun eli/tempel-temp-templates ()
   (alist-get major-mode eli/tempel-temp-templates))
